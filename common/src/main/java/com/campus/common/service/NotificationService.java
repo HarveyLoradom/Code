66019727c2a196464305177c014a2ee86ed8dba5
HarveyLoradom <130845816+HarveyLoradom@users.noreply.github.com>
2025-06-29 21:13:29 +0800

    Delete common directory

--- common/src/main/java/com/campus/common/service/NotificationService.java
+++
@@ -1,9 +0,0 @@
-package com.campus.common.service;
-
-import com.campus.common.entity.NotificationMessage;
-
-public interface NotificationService {
-    void sendNotification(NotificationMessage message);
-    void sendCourseSelectionNotification(String studentName, String courseName);
-    void sendScorePublishedNotification(String studentName, String courseName, Double score);
-} 
